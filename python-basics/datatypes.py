7b3b0f8f4866f6cf42d2c96d9095d221ba5fe92c
chclemo3-beep <chclemo3@gmail.com>
2026-02-11 09:48:44 +0300

    program add two numbers

---
+++ python-basics/datatypes.py
@@ -0,0 +1,11 @@
+# Name : Clement Ngotho
+# Date : 11/02/2026
+# Program to add two numbers
+
+first_number = 78
+second_number = 33
+
+sum_numbers = first_number + second_number
+
+
+print(sum_numbers)
