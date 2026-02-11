639c6b995b6000b29c3f0dffd9e5e6a2a793db81
chclemo3-beep <chclemo3@gmail.com>
2026-02-11 09:22:21 +0300

    first commit

---
+++ python-basics/challenge1.py
@@ -0,0 +1,9 @@
+university = "Jkuat"
+grade = "A"
+course = "plumbing"
+cluster_points = 80
+
+print(f"My favourie university is {university}")
+print(f"My kcse grade was {grade}")
+print(f"My favourie course is {course}")
+print(f"My cluster points are {cluster_points}")
---
+++ python-basics/hello.py
@@ -0,0 +1,13 @@
+# This is a comment
+print("Hello from Clement")
+# city/town
+print("I am from Kiambu")
+# My hobbies
+print("I like to swim,watch series,reading,writing")
+age = 28
+height = 2.33
+weight = 90
+
+print(f"I am {age} years old")
+print(f"I am also {height} metres tall")
+print(f"And {weight} kilograms")
