f214e0e8b4e51ba7aa2f53ebe1560a82860e7198
chclemo3-beep <chclemo3@gmail.com>
2026-02-24 10:16:27 +0300

    today

---
+++ python-basics/fileo.py
@@ -0,0 +1,22 @@
+new_file = open("student_data.txt","r+")
+
+#write to new file
+new_file.write("{ Clement Ngotho, ID :664512351 , chclemo3@gmail.com }")
+
+
+# read from the file
+new_file = open("student_data.txt","r+")
+
+data = new_file.read()
+
+print(data)
+
+
+new_file.close()
+
+
+import os 
+os.remove("remove.txt")
+
+pip.install 
+
