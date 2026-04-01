21c91eb5ed8ce9bc4cdb5cabe667209468eed83b
chclemo3-beep <chclemo3@gmail.com>
2026-04-01 08:50:32 +0300

    I am Clement

---
+++ WEB/src/main.js
@@ -0,0 +1,21 @@
+function message(){
+alert("Hello from Clement");
+
+}
+
+var fnumber,snumber,sumnumber
+function add(fnumber,snumber)
+{
+    sumnumber = fnumber + snumber
+    return sumnumber
+
+}
+
+function change text()
+{
+    var btn = document.getElementById("mybtn")
+    var span = document.getElementByID("output");
+    var textbox = document.getElementById("textbox")
+
+    textbox.style.color = "indigo"
+}
